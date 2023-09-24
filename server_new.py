9d7fb1e88f40a746742ce42bc3f21207b17a8a1f
Zhaiyuqing2003 <48752879+Zhaiyuqing2003@users.noreply.github.com>
2023-09-23 20:31:47 -0500

    update

---
+++ server_new.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+import time
+import json
+import sys
+import random
+import argparse
+
+
+# GLOBAL CONSTANTS
+
+T_GOSSIP = 5
+T_CLEANUP = 10
+
+# GLOBAL VARIABLES
+
+nodes = {
+    'A'
+}
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
